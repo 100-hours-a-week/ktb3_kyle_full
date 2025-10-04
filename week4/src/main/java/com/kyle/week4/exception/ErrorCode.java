b41a2f396dd8d7db3c163271b345cafb5fa844b1
bak3839 <bak3839@naver.com>
2025-10-05 00:36:26 +0900

    refactor: ErrorCode의 errorMessage 영어에서 한국어 반환으로 변경

--- week4/src/main/java/com/kyle/week4/exception/ErrorCode.java
+++ week4/src/main/java/com/kyle/week4/exception/ErrorCode.java
@@ -7,10 +7,10 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum ErrorCode {
-    NOT_FOUND_END_POINT(404, HttpStatus.NOT_FOUND, "not_found_api"),
-    DUPLICATE_EMAIL_ERROR(409, HttpStatus.CONFLICT, "duplicate_email"),
-    DUPLICATE_NICKNAME_ERROR(409, HttpStatus.CONFLICT, "duplicate_nickname"),
-    INTERNAL_SERVER_ERROR(500, HttpStatus.INTERNAL_SERVER_ERROR, "internal_server_error"),;
+    NOT_FOUND_END_POINT(404, HttpStatus.NOT_FOUND, "존재하지 않는 API입니다."),
+    DUPLICATE_EMAIL_ERROR(409, HttpStatus.CONFLICT, "이미 가입된 이메일입니다."),
+    DUPLICATE_NICKNAME_ERROR(409, HttpStatus.CONFLICT, "이미 가입된 닉네임입니다."),
+    INTERNAL_SERVER_ERROR(500, HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부 오류입니다.");
 
     private final Integer code;
     private final HttpStatus httpStatus;
